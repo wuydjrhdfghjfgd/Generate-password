6b199786b47846684227c27aebebed987ded4c44
wuydjrhdfghjfgd ( листик ) <119734607+wuydjrhdfghjfgd@users.noreply.github.com>
2024-01-27 13:46:32 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+string getPassword(int length){
+    string Password = "";
+    string characters = "aquickbrownfoxjumpsoverthelazydogAQUICKBROWNFOXJUMPSOVERTHELAZYDOG0123456789";
+    int charSize = characters.size();
+    srand(time(0));
+    int randomIndex;
+    for(int i=0 ; i<length ; i++){
+        randomIndex = rand() % charSize;
+        Password = Password + characters[randomIndex];
+    }
+    return Password;
+}
+
+
+
+int main() {
+
+    int length;
+    cout << "Enter the length of the password : ";
+    cin >> length;
+    string password = getPassword(length);
+    cout << "Generated password: " << password;
+
+
+    return 0;
+}
